af50541740f0b1e89f2aab8584e76bccc61cfeff
gumoraes <gustavo.smoraes2@gmail.com>
2023-02-07 00:13:12 -0300

    Adicionando repositório com arquivos de config

---
+++ init.lua
@@ -0,0 +1,7 @@
+require("configs.settings")
+require("plugins.plugins")
+require("configs.mappings")
+require("plugins.utils")
+require("plugins.feline")
+require("plugins.identblank")
+require("configs.telescope")
